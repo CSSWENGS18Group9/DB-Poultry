10f4f4269f1b1600bde11f993208a60626cfc979
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-05-19 17:21:20 +0800

    added comments

--- app/src/main/kotlin/org/db_poultry/ManageInventory.kt
+++ app/src/main/kotlin/org/db_poultry/ManageInventory.kt
@@ -2,6 +2,17 @@ package org.db_poultry
 
 import java.sql.*
 
+/**
+ * Adds a new record into the Supply table
+ *
+ * @param connect the Connection thing with SQL
+ * @param supplyID the supply's ID
+ * @param quantity the quantity of this supply
+ * @param unit the unit of this supply
+ * @param type the type of this supply
+ *
+ * @return an Int if record was inserted properly
+ */
 fun addSupply(connect: Connection, supplyID: String, quantity: Int, unit: Int, type: Int): Int {
     val query = "INSERT INTO Supply (Supply_ID, Quantity, Unit, Type) VALUES (?, ?, ?, ?)" // Query to be used in preparedStatement
 
@@ -20,6 +31,17 @@ fun addSupply(connect: Connection, supplyID: String, quantity: Int, unit: Int, t
     return success // Returns 1 if success
 }
 
+/**
+ * Updates an existing record in the Supply table
+ *
+ * @param connect the Connection thing with SQL
+ * @param supplyID the supply's ID
+ * @param quantity the quantity of this supply
+ * @param unit the unit of this supply
+ * @param type the type of this supply
+ *
+ * @return an Int if record was updated properly
+ */
 fun updateSupply(connect: Connection, supplyID: String, quantity: Int, unit: Int, type: Int): Int {
     val query = "UPDATE Supply SET Quantity = ?, unit = ?, type = ? WHERE Supply_ID = ?" // Query to be used in preparedStatement
 
