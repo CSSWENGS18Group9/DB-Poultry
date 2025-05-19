8f33c10a73b743bcdefcdaa79690d52569e5e6e0
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-05-19 20:44:02 +0800

    Changed parameters and queries to follow new Supply table format

--- database/ManageInventory.java
+++ database/ManageInventory.java
@@ -7,24 +7,24 @@ public class SupplyManage {
     /**
      * Adds a new record into the Supply table
      *
-     * @param connect  the Connection thing with SQL
-     * @param supplyID the supply's ID
-     * @param quantity the quantity of this supply
-     * @param unit     the unit of this supply
-     * @param type     the type of this supply
+     * @param connect       the Connection thing with SQL
+     * @param supplyPrefix  the prefix of this supply
+     * @param supplyID      the ID of this supply
+     * @param quantity      the quantity of this supply
+     * @param unit          the unit of this supply
      * @return an int if record was inserted properly
      */
-    public static int addSupply(Connection connect, String supplyID, int quantity, int unit, int type) {
-        String query = "INSERT INTO Supply (Supply_ID, Quantity, Unit, Type) VALUES (?, ?, ?, ?)"; // Query to be used in preparedStatement
+    public static int addSupply(Connection connect, String supplyPrefix, int supplyID, int quantity, int unit) {
+        String query = "INSERT INTO Supply (Supply_Prefix, Supply_ID, Quantity, Unit) VALUES (?, ?, ?, ?)"; // Query to be used in preparedStatement
 
         try {
             PreparedStatement preppedStatement = connect.prepareStatement(query); // preparedStatement for SQL stuff
 
             // Sets the values to be added
-            preppedStatement.setString(1, supplyID);
-            preppedStatement.setInt(2, quantity);
-            preppedStatement.setInt(3, unit);
-            preppedStatement.setInt(4, type);
+            preppedStatement.setString(1, supplyPrefix);
+            preppedStatement.setInt(2, supplyID);
+            preppedStatement.setInt(3, quantity);
+            preppedStatement.setInt(4, unit);
 
             int success = preppedStatement.executeUpdate(); // Executes query
 
@@ -40,15 +40,15 @@ public class SupplyManage {
         /**
          * Updates an existing record in the Supply table
          *
-         * @param connect  the Connection thing with SQL
-         * @param supplyID the supply's ID
-         * @param quantity the quantity of this supply
-         * @param unit     the unit of this supply
-         * @param type     the type of this supply
+         * @param connect       the Connection thing with SQL
+         * @param supplyPrefix  the prefix of this supply
+         * @param supplyID      the ID of this supply
+         * @param quantity      the quantity of this supply
+         * @param unit          the unit of this supply
          * @return an int if record was updated properly
          */
-        public static int updateSupply (Connection connect, String supplyID,int quantity, int unit, int type) {
-            String query = "UPDATE Supply SET Quantity = ?, unit = ?, type = ? WHERE Supply_ID = ?"; // Query to be used in preparedStatement
+        public static int updateSupply (Connection connect, String supplyPrefix, String supplyID,int quantity, int unit) {
+            String query = "UPDATE Supply SET Quantity = ?, unit = ? WHERE Supply_ID = ? AND Supply_Prefix = ?"; // Query to be used in preparedStatement
 
             try {
                 PreparedStatement preppedStatement = connect.prepareStatement(query); // preparedStatement for SQL stuff
@@ -56,8 +56,8 @@ public class SupplyManage {
                 // Sets the values to be updated
                 preppedStatement.setInt(1, quantity);
                 preppedStatement.setInt(2, unit);
-                preppedStatement.setInt(3, type);
-                preppedStatement.setString(4, supplyID);
+                preppedStatement.setString(3, supplyID);
+                preppedStatement.setString(4, supplyPrefix);
 
                 int success = preppedStatement.executeUpdate(); // Executes query
 
