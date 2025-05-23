1c1c8de77bc8669e4fafb65eb16ed6f2ffc4631e
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-05-23 17:10:01 +0800

    added missing Flock_Details_ID (or poultryID) to createPoultry()

--- app/src/main/java/org/db_poultry/db/poultry/Create.java
+++ app/src/main/java/org/db_poultry/db/poultry/Create.java
@@ -16,17 +16,18 @@ public class Create {
      * @param curCount      the current amount of chickens
      * @return a String which is the query with filled-in values
      */
-    public static String createPoultry(Connection connect, int flockID, Timestamp date, int depleted, int curCount) {
-        String query = "INSERT INTO Flock_Details (Flock_ID, FD_Date, Current_Count, Depleted_Count) VALUES (?,?, ?, ?)"; // Query to be used in preparedStatement
+    public static String createPoultry(Connection connect, int poultryID, int flockID, Timestamp date, int depleted, int curCount) {
+        String query = "INSERT INTO Flock_Details (Flock_Details_ID, Flock_ID, FD_Date, Current_Count, Depleted_Count) VALUES (?, ?, ?, ?, ?)"; // Query to be used in preparedStatement
 
         try {
             PreparedStatement preppedStatement = connect.prepareStatement(query); // preparedStatement for SQL stuff
 
             // Sets the values to be added
-            preppedStatement.setInt(1, flockID);
-            preppedStatement.setTimestamp(2, date);
-            preppedStatement.setInt(3, depleted);
-            preppedStatement.setInt(4, curCount);
+            preppedStatement.setInt(1, poultryID);
+            preppedStatement.setInt(2, flockID);
+            preppedStatement.setTimestamp(3, date);
+            preppedStatement.setInt(4, depleted);
+            preppedStatement.setInt(5, curCount);
 
             preppedStatement.executeUpdate(); // Executes query
 
