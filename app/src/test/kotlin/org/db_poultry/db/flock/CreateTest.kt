a91458ac1be36383a827386eb991504fded3b1b6
Zhean Ganituen <zr.gntn@gmail.com>
2025-05-23 12:57:43 -0400

    Fixed CreateTest

--- app/src/test/kotlin/org/db_poultry/db/flock/CreateTest.kt
+++ app/src/test/kotlin/org/db_poultry/db/flock/CreateTest.kt
@@ -4,26 +4,28 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 import org.db_poultry.db.DBConnect
 import org.db_poultry.App
-import org.db_poultry.db.flock.*
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
 
 class CreateTest {
     private var jdbcURL: String
-    private var connection: DBConnect
+    private var conn: DBConnect
 
     init {
         val app = App()
 
         app.getDotEnv()
 
-        jdbcURL = "jdbc:postgresql://localhost:$app.databasePort/$app.databaseName"
-        connection = DBConnect(jdbcURL, app.databaseName, app.databasePass)
+        jdbcURL = "jdbc:postgresql://localhost:${app.databasePort}/${app.databaseName}"
+        conn = DBConnect(jdbcURL, app.databaseName, app.databasePass)
     }
 
     @Test
     fun testCreateFlock() {
-        val create = Create()
-        assertEquals(create.createFlock(connection, "1999-01-01"), "INSERT INTO Flock (Starting_Date) VALUES (1999-01-01)")
+        val timestamp = Timestamp(SimpleDateFormat("yyyy-MM-dd").parse("1999-01-01").time)
+        val result = Create.createFlock(conn.conn, timestamp)
+        assertTrue(result?.contains("insert into flock", ignoreCase = true) == true)
     }
-
 }
+
 
