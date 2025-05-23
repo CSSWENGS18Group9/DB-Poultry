35eb086da8f23037c714b5359ac4f207086d89ad
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-05-24 00:01:37 +0800

    Fixed SQL queries.

--- app/src/main/java/org/db_poultry/db/flock/Create.java
+++ app/src/main/java/org/db_poultry/db/flock/Create.java
@@ -13,19 +13,17 @@ public class Create {
      * Adds a new record into the Flock table
      *
      * @param connect       the Connection thing with SQL
-     * @param flockID       the ID of this flock
      * @param startDate     the starting date of this flock
      * @return a String which is the query with filled-in values
      */
-    public static String createFlock(Connection connect, int flockID, Timestamp startDate) {
-        String query = "INSERT INTO Flock (Flock_ID, Starting_Date) VALUES (?,?)"; // Query to be used in preparedStatement
+    public static String createFlock(Connection connect, Timestamp startDate) {
+        String query = "INSERT INTO Flock (Starting_Date) VALUES (?)"; // Query to be used in preparedStatement
 
         try {
             PreparedStatement preppedStatement = connect.prepareStatement(query); // preparedStatement for SQL stuff
 
             // Sets the values to be added
-            preppedStatement.setInt(1, flockID);
-            preppedStatement.setTimestamp(2, startDate);
+            preppedStatement.setTimestamp(1, startDate);
 
             preppedStatement.executeUpdate(); // Executes query
 
--- app/src/main/java/org/db_poultry/db/poultry/Create.java
+++ app/src/main/java/org/db_poultry/db/poultry/Create.java
@@ -13,25 +13,23 @@ public class Create {
      * Adds a new record into the Poultry table
      *
      * @param connect       the Connection thing with SQL
-     * @param poultryID     the ID of this poultry
      * @param flockID       the flock ID this poultry is connected to
      * @param date          the starting date of this flock
      * @param depleted      the amount of dead chicken
      * @param curCount      the current amount of chickens
      * @return a String which is the query with filled-in values
      */
-    public static String createPoultry(Connection connect, int poultryID, int flockID, Timestamp date, int depleted, int curCount) {
-        String query = "INSERT INTO Flock_Details (Flock_Details_ID, Flock_ID, FD_Date, Current_Count, Depleted_Count) VALUES (?, ?, ?, ?, ?)"; // Query to be used in preparedStatement
+    public static String createPoultry(Connection connect, int flockID, Timestamp date, int depleted, int curCount) {
+        String query = "INSERT INTO Flock_Details (Flock_ID, FD_Date, Current_Count, Depleted_Count) VALUES (?, ?, ?, ?, ?)"; // Query to be used in preparedStatement
 
         try {
             PreparedStatement preppedStatement = connect.prepareStatement(query); // preparedStatement for SQL stuff
 
             // Sets the values to be added
-            preppedStatement.setInt(1, poultryID);
-            preppedStatement.setInt(2, flockID);
-            preppedStatement.setTimestamp(3, date);
-            preppedStatement.setInt(4, depleted);
-            preppedStatement.setInt(5, curCount);
+            preppedStatement.setInt(1, flockID);
+            preppedStatement.setTimestamp(2, date);
+            preppedStatement.setInt(3, depleted);
+            preppedStatement.setInt(4, curCount);
 
             preppedStatement.executeUpdate(); // Executes query
 
