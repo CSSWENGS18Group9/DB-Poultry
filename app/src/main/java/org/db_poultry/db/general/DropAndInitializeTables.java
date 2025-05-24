2798bc20c371ac1c128c0156ab6060014e3c6276
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-05-24 14:42:42 +0800

    Added drop query

--- app/src/main/java/org/db_poultry/db/general/DropAndInitializeTables.java
+++ app/src/main/java/org/db_poultry/db/general/DropAndInitializeTables.java
@@ -9,15 +9,17 @@ import static org.db_poultry.errors.GenerateErrorMessageKt.generateErrorMessage;
 public class DropAndInitializeTables {
 
     public static String initTables(Connection connect) {
-        String completeQuery = "CREATE TABLE Flock (Flock_ID SERIAL PRIMARY KEY, Starting_Date DATE)"; // First query that creates Flock table
+        String completeQuery = "DROP TABLE IF EXISTS Flock, Flock_Details;"; // First query that drops the Flock and Flock_Details tables
 
         try {
             Statement statement = connect.createStatement(); // statement for SQL stuff
 
             statement.execute(completeQuery); // Executes query
 
-            completeQuery = "CREATE TABLE Flock_Details (Flock_Details_ID SERIAL PRIMARY KEY, Flock_ID SERIAL, FD_Date DATE, Current_Count INTEGER, Depleted_Count INTEGER, FOREIGN KEY (Flock_ID) REFERENCES Flock(Flock_ID))"; // Second query that creates Flock_Details table
+            completeQuery = "CREATE TABLE Flock (Flock_ID SERIAL PRIMARY KEY, Starting_Date DATE)"; // Second query that creates Flock table
+            statement.execute(completeQuery); // Executes query
 
+            completeQuery = "CREATE TABLE Flock_Details (Flock_Details_ID SERIAL PRIMARY KEY, Flock_ID SERIAL, FD_Date DATE, Current_Count INTEGER, Depleted_Count INTEGER, FOREIGN KEY (Flock_ID) REFERENCES Flock(Flock_ID))"; // Third query that creates Flock_Details table
             statement.execute(completeQuery); // Executes query
 
             statement.close(); // Closes statement
