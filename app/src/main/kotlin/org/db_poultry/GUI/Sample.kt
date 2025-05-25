0e2b6c060c83f255b7e9f09ac68504b1a30810e8
Dattebayo2505 <seankyled1st@gmail.com>
2025-05-26 02:30:52 +0800

    add: font-to-use (Helvetica) & font loading

--- app/src/main/kotlin/org/db_poultry/GUI/Sample.kt
+++ app/src/main/kotlin/org/db_poultry/GUI/Sample.kt
@@ -6,8 +6,12 @@ import javafx.stage.Stage
 import javafx.scene.Parent
 import javafx.scene.Scene
 
+import javafx.scene.text.Font
+
 class Sample : Application() {
     override fun start(primaryStage: Stage) {
+        loadCustomFonts()
+
         try {
             val loader = FXMLLoader(javaClass.getResource("/login.fxml"))
             val root = loader.load<Parent>()
@@ -22,4 +26,31 @@ class Sample : Application() {
             e.printStackTrace()
         }
     }
+
+private fun loadCustomFonts() {
+    val fontFiles = listOf(
+        "HelveticaNowDisplay-Black.ttf",
+        "HelveticaNowDisplay-Bold.ttf",
+        "HelveticaNowDisplay-BoldIta.ttf",
+        "HelveticaNowDisplay-ExtBdIta.ttf",
+        "HelveticaNowDisplay-ExtBlk.ttf",
+        "HelveticaNowDisplay-ExtBlkIta.ttf",
+        "HelveticaNowDisplay-ExtLt.ttf",
+        "HelveticaNowDisplay-ExtraBold.ttf",
+        "HelveticaNowDisplay-Hairline.ttf",
+        "HelveticaNowDisplay-HairlineIta.ttf",
+        "HelveticaNowDisplay-Light.ttf",
+        "HelveticaNowDisplay-LightIta.ttf",
+        "HelveticaNowDisplay-MedIta.ttf",
+        "HelveticaNowDisplay-Medium.ttf",
+        "HelveticaNowDisplay-RegIta.ttf",
+        "HelveticaNowDisplay-Regular.ttf",
+        "HelveticaNowDisplay-Thin.ttf",
+        "HelveticaNowDisplay-ThinIta.ttf"
+    )
+    for (fontFile in fontFiles) {
+        Font.loadFont(javaClass.getResourceAsStream("/fonts/$fontFile"), 15.0)
+    }
+}
+
 }
