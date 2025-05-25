99833fc00383404e84b67c9cb72843920e98a08a
Zhean Ganituen <zr.gntn@gmail.com>
2025-05-25 14:42:37 -0400

    Updated the SQL query here (removed AS part)

--- app/src/main/java/org/db_poultry/db/flock/View.java
+++ app/src/main/java/org/db_poultry/db/flock/View.java
@@ -25,7 +25,7 @@ public class View {
         HashMap<K, FlockComplete> result = new HashMap<>();
 
         String sql = "SELECT f.flock_id, f.starting_count, f.starting_date, " +
-                "fd.flock_details_id AS fd_id, fd.fd_date, fd.depleted_count " +
+                "fd.flock_details_id, fd.fd_date, fd.depleted_count " +
                 "FROM Flock f INNER JOIN Flock_Details fd ON f.flock_id = fd.flock_id";
 
         try (Statement stmt = con.createStatement();
