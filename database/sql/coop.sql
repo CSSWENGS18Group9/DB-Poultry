542c6d4590c5591b7fb88b95e0306eac923e289c
OutForMilks <stephen_borja_a@dlsu.edu.ph>
2025-05-25 13:09:49 +0800

    Update coop.sql
    IMPORTANT: I added delete on cascade
    It should run properly this time,
    I added more restrictions

--- database/sql/coop.sql
+++ database/sql/coop.sql
@@ -1,15 +1,16 @@
 CREATE TABLE Flock (
     Flock_ID SERIAL PRIMARY KEY,
 
-    Starting_Count INTEGER CHECK (Starting_Count > 0),
-    Starting_Date UNIQUE DATE
+    Starting_Count INTEGER CHECK (Starting_Count > 0) NOT NULL,
+    Starting_Date DATE UNIQUE NOT NULL
 );
 
 CREATE TABLE Flock_Details (
     Flock_Details_ID SERIAL PRIMARY KEY,
+    Flock_ID INTEGER NOT NULL,
 
-    FD_Date UNIQUE DATE,
+    FD_Date DATE UNIQUE NOT NULL,
     Depleted_Count INTEGER CHECK (Depleted_Count >= 0),
 
-    FOREIGN KEY (Flock_ID) REFERENCES Flock(Flock_ID)
+    FOREIGN KEY (Flock_ID) REFERENCES Flock(Flock_ID) ON DELETE CASCADE
 );
