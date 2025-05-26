18c219d43edea76c9fae5809b912df9482a12ea5
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-05-26 17:55:36 +0800

    Added GetCumulativeDepletedCount.java, which gets the total depleted count from a specified Flock ID

---
+++ app/src/main/java/org/db_poultry/db/flock_detailsDAO/GetCumulativeDepletedCount.java
@@ -0,0 +1,40 @@
+package org.db_poultry.db.flock_detailsDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.db_poultry.errors.GenerateErrorMessageKt.generateErrorMessage;
+
+public class GetCumulativeDepletedCount {
+
+    public static int cumulativeDepletedCount(Connection connect, int flockID) {
+        String incompleteQuery = "SELECT SUM(Depleted_Count) AS Total_Count_Depleted FROM Flock_Details WHERE Flock_ID = ?"; // Query to be used in preparedStatement
+
+        try {
+            PreparedStatement preppedStatement = connect.prepareStatement(incompleteQuery); // preparedStatement for SQL stuff
+
+            // Sets the values to be added
+            preppedStatement.setInt(1, flockID);
+
+            ResultSet result = preppedStatement.executeQuery(); // Executes query and stores it into a ResultSet
+
+            int sum = 0; // Gets total depleted count
+
+            while (result.next()) { // Gets results per row from the SQL output
+                sum = result.getInt("Total_Count_Depleted"); // Gets the output from the column I named Total_Count_Depleted
+            }
+
+            result.close(); // CLoses result
+            preppedStatement.close(); // Closes preparedStatement
+
+            return sum; // Returns the total depleted count
+
+        } catch (SQLException e) {
+            generateErrorMessage("Error in `cumulativeDepletedCount()`.", "SQLException occurred.", "", e);
+            return -1;
+        }
+
+    }
+}
