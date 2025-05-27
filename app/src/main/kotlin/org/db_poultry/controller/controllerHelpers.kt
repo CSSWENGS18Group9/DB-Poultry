329c2ede56826ff410421fb7c2d2c8ccee2c6330
OutForMilks <stephen_borja_a@dlsu.edu.ph>
2025-05-27 17:36:26 +0800

    Update controllerHelpers.kt

--- app/src/main/kotlin/org/db_poultry/controller/controllerHelpers.kt
+++ app/src/main/kotlin/org/db_poultry/controller/controllerHelpers.kt
@@ -4,6 +4,8 @@ import org.db_poultry.db.flockDAO.ReadFlock
 import org.db_poultry.errors.generateErrorMessage
 import org.db_poultry.pojo.FlockComplete
 import java.sql.Connection
+import java.sql.Date
+import java.sql.SQLException
 
 fun <K> validateAndRead(
     connection: Connection?, reader: (Connection) -> HashMap<K, FlockComplete>?, callerName: String
@@ -28,7 +30,7 @@ fun <K> validateAndRead(
         return null
     }
 
-    return flocks;
+    return flocks
 }
 
 fun validateAndReadByID(connection: Connection?) =
@@ -36,3 +38,31 @@ fun validateAndReadByID(connection: Connection?) =
 
 fun validateAndReadByDates(connection: Connection?) =
     validateAndRead(connection, ReadFlock::allByDate, "validateAndReadByDates")
+
+fun checkDateInbetween(connect: Connection, inputtedDate: Date): Int {
+    val incompleteQuery = "SELECT COUNT(*) AS overlaps FROM Flock JOIN (SELECT Flock_ID, MAX(FD_Date) as endDate FROM Flock_Details GROUP BY Flock_ID) Details ON Flock.Flock_ID = Details.Flock_ID WHERE ? BETWEEN Flock.Starting_Date AND Details.endDate" // Query to be used in preparedStatement
+    // If it returns anything other than a 0 then there's overlap
+
+    try {
+        val preppedStatement = connect.prepareStatement(incompleteQuery) // preparedStatement for SQL stuff
+
+        // Sets the values to be added
+        preppedStatement.setDate(1, inputtedDate)
+
+        val result = preppedStatement.executeQuery() // Executes query and stores it into a ResultSet
+
+        var overlap = 0 // Gets number of overlap
+
+        while (result.next()) { // Gets results per row from the SQL output
+            overlap = result.getInt("overlaps") // Gets the output from the column I named Total_Count_Depleted
+        }
+
+        result.close() // CLoses result
+        preppedStatement.close() // Closes preparedStatement
+
+        return overlap // Returns the total depleted count
+    } catch (e: SQLException) {
+        generateErrorMessage("Error in checkDateInbetween().", "SQLException occurred.", "", e)
+        return -1
+    }
+}
