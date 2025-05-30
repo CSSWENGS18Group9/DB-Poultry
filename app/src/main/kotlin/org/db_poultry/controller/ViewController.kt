154ce4bcd0a108c08a15b7ed5ef7843fec497e4f
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-05-31 02:25:20 +0800

    Added to ViewController.kt

--- app/src/main/kotlin/org/db_poultry/controller/ViewController.kt
+++ app/src/main/kotlin/org/db_poultry/controller/ViewController.kt
@@ -1,12 +1,23 @@
 package org.db_poultry.controller
 
 import javafx.fxml.FXML
+import javafx.fxml.Initializable
 import javafx.scene.control.Button
 import javafx.scene.control.ChoiceBox
+import javafx.scene.layout.AnchorPane
 import javafx.scene.text.Text
+import org.db_poultry.Util.GeneralUtil
+import org.db_poultry.db.DBConnect
+import org.db_poultry.db.flockDAO.ReadFlock
+import java.net.URL
+import java.util.ResourceBundle
+import kotlin.collections.isNotEmpty
 
 
-class ViewController {
+class ViewController: Initializable {
+
+    @FXML
+    lateinit var selectFlockAnchorPane: AnchorPane
 
     @FXML
     private lateinit var viewSelectFlockChoiceBox: ChoiceBox<Any>
@@ -20,6 +31,25 @@ class ViewController {
     @FXML
     fun switchToViewFlockDetails() {
         println("Switching to view flock details")
-        
+        GeneralUtil.loadContentView(selectFlockAnchorPane, "/fxml/content_view_flock_details.fxml")
+    }
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+
+        val flockMap = ReadFlock.allByID(DBConnect.getConnection())
+
+        val dateList = mutableListOf<String>()
+
+        if (flockMap != null && flockMap.isNotEmpty()) {
+            val dates = flockMap.values.map { it.flock.startingDate.toString() }
+            dateList.addAll(dates)
+            println("Flock starting dates: $dates") // DEBUGGING
+        } else {
+            println("No data found.")
+        }
+
+        viewSelectFlockChoiceBox.items.clear()
+        viewSelectFlockChoiceBox.items.addAll(dateList)
+        viewSelectFlockChoiceBox.value = "Select a flock"
     }
 }
