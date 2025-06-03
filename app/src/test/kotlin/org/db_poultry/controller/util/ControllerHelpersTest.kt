36dc2b9ad76af329d4e4b92ea7410b91ca1239ed
Zhean Ganituen <zr.gntn@gmail.com>
2025-06-02 21:16:01 -0400

    HOTFIX: fixed `DBConnection` errors at `ControllerHelpersTest.kt`

--- app/src/test/kotlin/org/db_poultry/controller/util/ControllerHelpersTest.kt
+++ app/src/test/kotlin/org/db_poultry/controller/util/ControllerHelpersTest.kt
@@ -7,27 +7,30 @@ import org.db_poultry.db.flockDAO.CreateFlock
 import org.db_poultry.db.flockDetailsDAO.CreateFlockDetails
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.sql.Connection
 import java.sql.Date
 
 class ControllerHelpersTest {
     private var jdbcURL: String
-    private var conn: DBConnect
+    private val conn: Connection
 
     init {
         val app = App()
         app.getDotEnv()
         jdbcURL = "jdbc:postgresql://localhost:${app.databasePort}/${app.databaseName}"
-        conn = DBConnect(jdbcURL, app.databaseName, app.databasePass)
-        cleanTables(conn.getConnection())
+
+        DBConnect.init(jdbcURL, app.databaseName, app.databasePass)
+        conn = DBConnect.getConnection()!!
+        cleanTables(conn)
     }
 
     @Test
     fun testValidateAndReadByIDWithValidData() {
-        val connection = conn.getConnection()
-        requireNotNull(connection)
+        
+        requireNotNull(conn)
         // Insert a flock
-        CreateFlock.createFlock(connection, 100, Date.valueOf("2025-01-01"))
-        val result = validateAndReadByID(connection)
+        CreateFlock.createFlock(conn, 100, Date.valueOf("2025-01-01"))
+        val result = validateAndReadByID(conn)
         assertNotNull(result)
         assertTrue(result!!.isNotEmpty())
     }
@@ -40,18 +43,18 @@ class ControllerHelpersTest {
 
     @Test
     fun testValidateAndReadByIDWithNoData() {
-        val connection = conn.getConnection()
-        cleanTables(connection)
-        val result = validateAndReadByID(connection)
+        
+        cleanTables(conn)
+        val result = validateAndReadByID(conn)
         assertNull(result)
     }
 
     @Test
     fun testValidateAndReadByDatesWithValidData() {
-        val connection = conn.getConnection()
-        requireNotNull(connection)
-        CreateFlock.createFlock(connection, 100, Date.valueOf("2025-01-01"))
-        val result = validateAndReadByDates(connection)
+        
+        requireNotNull(conn)
+        CreateFlock.createFlock(conn, 100, Date.valueOf("2025-01-01"))
+        val result = validateAndReadByDates(conn)
         assertNotNull(result)
         assertTrue(result!!.isNotEmpty())
     }
@@ -64,28 +67,28 @@ class ControllerHelpersTest {
 
     @Test
     fun testValidateAndReadByDatesWithNoData() {
-        val connection = conn.getConnection()
-        cleanTables(connection)
-        val result = validateAndReadByDates(connection)
+        
+        cleanTables(conn)
+        val result = validateAndReadByDates(conn)
         assertNull(result)
     }
 
     @Test
     fun testCheckDateInbetweenWithNoOverlap() {
-        val connection = conn.getConnection()
-        cleanTables(connection)
-        CreateFlock.createFlock(connection, 100, Date.valueOf("2025-01-01"))
-        val overlap = connection?.let { checkDateInbetween(it, Date.valueOf("2024-12-31")) }
+        
+        cleanTables(conn)
+        CreateFlock.createFlock(conn, 100, Date.valueOf("2025-01-01"))
+        val overlap = checkDateInbetween(conn, Date.valueOf("2024-12-31"))
         assertEquals(0, overlap)
     }
 
     @Test
     fun testCheckDateInbetweenWithOverlap() {
-        val connection = conn.getConnection()
-        cleanTables(connection)
-        CreateFlock.createFlock(connection, 100, Date.valueOf("2025-01-01"))
-        val overlap = connection?.let { checkDateInbetween(it, Date.valueOf("2025-01-01")) }
-        overlap?.let { assertTrue(it > 0) }
+        
+        cleanTables(conn)
+        CreateFlock.createFlock(conn, 100, Date.valueOf("2025-01-01"))
+        val overlap = checkDateInbetween(conn, Date.valueOf("2025-01-01"))
+        assertTrue(overlap > 0)
     }
 
     @Test
@@ -100,41 +103,37 @@ class ControllerHelpersTest {
         val dateFDFive = Date.valueOf("1000-08-01")
         val dateFDSix = Date.valueOf("1000-09-01")
 
-        val connection = conn.getConnection()
+        
 
-        CreateFlock.createFlock(conn.getConnection(), 100, dateFlockOne)
+        CreateFlock.createFlock(conn, 100, dateFlockOne)
 
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 1, dateFDOne, 5)
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 1, dateFDTwo, 10)
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 1, dateFDThree, 15)
+        CreateFlockDetails.createFlockDetails(conn, 1, dateFDOne, 5)
+        CreateFlockDetails.createFlockDetails(conn, 1, dateFDTwo, 10)
+        CreateFlockDetails.createFlockDetails(conn, 1, dateFDThree, 15)
 
-        CreateFlock.createFlock(conn.getConnection(), 100, dateFlockTwo)
+        CreateFlock.createFlock(conn, 100, dateFlockTwo)
 
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 2, dateFDFour, 5)
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 2, dateFDFive, 10)
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 2, dateFDSix, 15)
+        CreateFlockDetails.createFlockDetails(conn, 2, dateFDFour, 5)
+        CreateFlockDetails.createFlockDetails(conn, 2, dateFDFive, 10)
+        CreateFlockDetails.createFlockDetails(conn, 2, dateFDSix, 15)
 
         assertEquals(
-            connection?.let {
-                checkDateInbetween(it, Date.valueOf("1000-05-02")) },
+            checkDateInbetween(conn, Date.valueOf("1000-05-02")),
             0
         )
 
         assertEquals(
-            connection?.let {
-                checkDateInbetween(it, Date.valueOf("1000-05-31")) },
+            checkDateInbetween(conn, Date.valueOf("1000-05-31")),
             0
         )
 
         assertEquals(
-            connection?.let {
-                checkDateInbetween(it, Date.valueOf("1000-09-02")) },
+            checkDateInbetween(conn, Date.valueOf("1000-09-02")),
             0
         )
 
         assertEquals(
-            connection?.let {
-                checkDateInbetween(it, Date.valueOf("1000-01-01")) },
+            checkDateInbetween(conn, Date.valueOf("1000-01-01")),
             0
         )
     }
@@ -151,56 +150,51 @@ class ControllerHelpersTest {
         val dateFDFive = Date.valueOf("1000-08-01")
         val dateFDSix = Date.valueOf("1000-09-01")
 
-        val connection = conn.getConnection()
+        
 
-        CreateFlock.createFlock(conn.getConnection(), 100, dateFlockOne)
+        CreateFlock.createFlock(conn, 100, dateFlockOne)
 
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 1, dateFDOne, 5)
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 1, dateFDTwo, 10)
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 1, dateFDThree, 15)
+        CreateFlockDetails.createFlockDetails(conn, 1, dateFDOne, 5)
+        CreateFlockDetails.createFlockDetails(conn, 1, dateFDTwo, 10)
+        CreateFlockDetails.createFlockDetails(conn, 1, dateFDThree, 15)
 
-        CreateFlock.createFlock(conn.getConnection(), 100, dateFlockTwo)
+        CreateFlock.createFlock(conn, 100, dateFlockTwo)
 
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 2, dateFDFour, 5)
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 2, dateFDFive, 10)
-        CreateFlockDetails.createFlockDetails(conn.getConnection(), 2, dateFDSix, 15)
+        CreateFlockDetails.createFlockDetails(conn, 2, dateFDFour, 5)
+        CreateFlockDetails.createFlockDetails(conn, 2, dateFDFive, 10)
+        CreateFlockDetails.createFlockDetails(conn, 2, dateFDSix, 15)
 
         assertEquals(
             1,
-            connection?.let {
-                checkDateInbetween(it, Date.valueOf("1000-05-01")) }
+            checkDateInbetween(conn, Date.valueOf("1000-05-01"))
         )
 
         assertEquals(
             1,
-            connection?.let {
-                checkDateInbetween(it, Date.valueOf("1000-06-01")) }
+            checkDateInbetween(conn, Date.valueOf("1000-06-01"))
         )
 
         assertEquals(
             1,
-            connection?.let {
-                checkDateInbetween(it, Date.valueOf("1000-07-12")) }
+            checkDateInbetween(conn, Date.valueOf("1000-07-12"))
         )
 
         assertEquals(
             1,
-            connection?.let {
-                checkDateInbetween(it, Date.valueOf("1000-02-01")) }
+            checkDateInbetween(conn, Date.valueOf("1000-02-01"))
         )
 
         assertEquals(
             1,
-            connection?.let {
-                checkDateInbetween(it, Date.valueOf("1000-02-05")) }
+            checkDateInbetween(conn, Date.valueOf("1000-02-05"))
         )
     }
 
     @Test
     fun testCheckDateInbetweenSQLException() {
-        val connection = conn.getConnection()
-        connection?.close() // force SQLException
-        val overlap = connection?.let { checkDateInbetween(it, Date.valueOf("2025-01-01")) }
+
+        conn.close() // force SQLException
+        val overlap = checkDateInbetween(conn, Date.valueOf("2025-01-01"))
         assertEquals(-1, overlap)
     }
 }
