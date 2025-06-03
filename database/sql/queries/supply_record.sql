4e52b761e2f07b2cf803400509b0f460e814329f
Zhean Ganituen <zr.gntn@gmail.com>
2025-06-03 06:28:00 -0400

    Added SQL query for creating and viewing SQL queries

---
+++ database/sql/queries/supply_record.sql
@@ -0,0 +1,47 @@
+-- Create a new Supply record
+INSERT INTO
+        Supply_Record (
+                Supply_Type_ID,
+                Current_Quantity,
+                Deleted,
+                Added,
+                Unit,
+                SR_Date,
+                Retrieved
+        )
+VALUES
+        (?, ?, ?, ?, ?, ?, ?);
+
+-- View all Supply Records for a specific date
+SELECT
+        st.Supply_Name,
+        sr.Current_Quantity,
+        sr.Deleted,
+        sr.Added,
+        sr.Unit,
+        sr.SR_Date,
+        sr.Retrieved
+FROM
+        Supply_Record sr
+        JOIN Supply_Type st ON sr.Supply_Type_ID = st.Supply_Type_ID
+WHERE
+        sr.SR_Date = ?;
+
+-- View all Supply Records for a specific date and type (id)
+-- View all Supply Records for a specific supply type and date
+SELECT
+        st.Supply_Name,
+        sr.Current_Quantity,
+        sr.Deleted,
+        sr.Added,
+        sr.Unit,
+        sr.SR_Date,
+        sr.Retrieved
+FROM
+        Supply_Record sr
+        JOIN Supply_Type st ON sr.Supply_Type_ID = st.Supply_Type_ID
+WHERE
+        -- for supply name
+        -- sr.Supply_Name = ? 
+        sr.Supply_Type_ID = ?
+        AND sr.SR_Date = ?;
