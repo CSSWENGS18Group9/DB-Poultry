3776a29fc4a2b230a0e5f6ee38a61d29b4aed785
Zhean Ganituen <zr.gntn@gmail.com>
2025-06-05 00:23:15 -0400

    Changed class name

---
+++ app/src/main/java/org/db_poultry/db/supplyRecordDAO/CreateSupplyRecord.java
@@ -0,0 +1,48 @@
+package org.db_poultry.db.supplyRecordDAO;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.db_poultry.errors.GenerateErrorMessageKt.generateErrorMessage;
+
+public class CreateSupplyRecord {
+
+    public static String createSupplyRecord(Connection connect, int supplyType, Date srDate, float added,
+            float consumed) {
+        // TODO: Add checker later on if the SupplyType DOES exist
+        // ---
+            
+        String query = "INSERT INTO Supply_Record (Supply_Type_ID, SR_Date, Added, Consumed, Retrieved) VALUES (?, ?, ?, ?, ?)";
+        String filledQuery = String.format(
+                "INSERT INTO Supply_Record (Supply_Type_ID, SR_Date, Added, Consumed, Retrieved) VALUES (%d, '%s', %.2f, %.2f, %b);",
+                supplyType, srDate, added, consumed, false);
+
+        try {
+            PreparedStatement preparedStatement = connect.prepareStatement(query);
+
+            BigDecimal addedDecimal = BigDecimal.valueOf(added);
+            BigDecimal consumedDecimal = BigDecimal.valueOf(consumed);
+
+            preparedStatement.setInt(1, supplyType);
+            preparedStatement.setDate(2, srDate);
+            preparedStatement.setBigDecimal(3, addedDecimal);
+            preparedStatement.setBigDecimal(4, consumedDecimal);
+            preparedStatement.setBoolean(5, false); // always assumes Retrieved is False at creation
+
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            return filledQuery;
+        } catch (SQLException e) {
+            generateErrorMessage("Error in `createSupplyRecord()` in `createSupplyRecord.",
+                    "SQL Exception error occured",
+                    "",
+                    e);
+
+            return null;
+        }
+    }
+}
