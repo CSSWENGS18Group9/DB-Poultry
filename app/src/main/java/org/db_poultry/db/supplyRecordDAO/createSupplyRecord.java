8f60865650bf0c6bded894e294a11506fc5fcac7
Zhean Ganituen <zr.gntn@gmail.com>
2025-06-05 00:04:29 -0400

    Added createSupplyRecord

---
+++ app/src/main/java/org/db_poultry/db/supplyRecordDAO/createSupplyRecord.java
@@ -0,0 +1,44 @@
+package org.db_poultry.db.supplyRecordDAO;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.db_poultry.errors.GenerateErrorMessageKt.generateErrorMessage;
+
+public class createSupplyRecord {
+
+    public static String createSupplyRecord(Connection connect, int supplyType, Date srDate, float added,
+                                            float consumed) {
+        // TODO: Add checker later on if the SupplyType DOES exist
+        // ---
+
+        String filledQuery = "INSERT INTO Supply_Record (Supply_Type_ID, SR_Date, Added, Consumed, Retrieved) VALUES (" + supplyType + ", " + srDate + ", " + added + ", " + consumed + ", Retrieved);";
+        String query = "INSERT INTO Supply_Record (Supply_Type_ID, SR_Date, Added, Consumed, Retrieved) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = connect.prepareStatement(query);
+
+            BigDecimal addedDecimal = BigDecimal.valueOf(added);
+            BigDecimal consumedDecimal = BigDecimal.valueOf(consumed);
+
+            preparedStatement.setInt(1, supplyType);
+            preparedStatement.setDate(2, srDate);
+            preparedStatement.setBigDecimal(3, addedDecimal);
+            preparedStatement.setBigDecimal(4, consumedDecimal);
+            preparedStatement.setBoolean(5, false); // always assumes Retrieved is False at creation
+
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            return filledQuery;
+        } catch (SQLException e) {
+            generateErrorMessage("Error in `createSupplyRecord()` in `createSupplyRecord.",
+                    "SQL Exception error occured",
+                    "",
+                    e);
+        }
+    }
+}
