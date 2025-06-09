49049e477cb5f56cb777b3b6cc0d78d5f5e03f8f
Zhean Ganituen <zr.gntn@gmail.com>
2025-06-09 13:32:33 -0400

    Fixed errors in the SQL query for Supply Record

--- database/sql/queries/supply_record.sql
+++ database/sql/queries/supply_record.sql
@@ -1,48 +1,69 @@
 -- Create a new Supply record
-INSERT INTO Supply_Record (Supply_Type_ID,
-                           SR_Date,
-                           Added,
-                           Consumed,
-                           Retrieved)
-VALUES (?, ?, ?, ?, ?);
+INSERT INTO
+  Supply_Record (
+    Supply_Type_ID,
+    SR_Date,
+    Added,
+    Consumed,
+    Retrieved
+  )
+VALUES
+  (?, ?, ?, ?, ?);
 
 -- View all Supply Records for a specific date
-SELECT sr.Supply_ID,
-       sr.Supply_Type_ID sr.SR_Date, st.Supply_Name,
-       st.Unit,
-       sr.Added,
-       sr.Consumed,
-       sr.Retrieved
-FROM Supply_Record sr
-         JOIN Supply_Type st ON sr.Supply_Type_ID = st.Supply_Type_ID
-WHERE sr.SR_Date = ?;
+SELECT
+  sr.Supply_ID,
+  sr.Supply_Type_ID,
+  sr.SR_Date,
+  st.Supply_Name,
+  st.Unit,
+  sr.Added,
+  sr.Consumed,
+  sr.Retrieved
+FROM
+  Supply_Record sr
+  JOIN Supply_Type st ON sr.Supply_Type_ID = st.Supply_Type_ID
+WHERE
+  sr.SR_Date = ?;
 
 -- View all Supply Records for a specific supply type name
-SELECT sr.Supply_ID,
-       sr.Supply_Type_ID sr.SR_Date, st.Supply_Name,
-       st.Unit,
-       sr.Added,
-       sr.Consumed,
-       sr.Retrieved
-FROM Supply_Record sr
-         JOIN Supply_Type st ON sr.Supply_Type_ID = st.Supply_Type_ID
-WHERE st.Supply_Name = ?;
+SELECT
+  sr.Supply_ID,
+  sr.Supply_Type_ID,
+  sr.SR_Date,
+  st.Supply_Name,
+  st.Unit,
+  sr.Added,
+  sr.Consumed,
+  sr.Retrieved
+FROM
+  Supply_Record sr
+  JOIN Supply_Type st ON sr.Supply_Type_ID = st.Supply_Type_ID
+WHERE
+  st.Supply_Name = ?;
 
 -- View a SINGLE Supply Records for a specific supply name and date
-SELECT sr.Supply_ID,
-       sr.Supply_Type_ID sr.SR_Date, st.Supply_Name,
-       st.Unit,
-       sr.Added,
-       sr.Consumed,
-       sr.Retrieved
-FROM Supply_Record sr
-         JOIN Supply_Type st ON sr.Supply_Type_ID = st.Supply_Type_ID
-WHERE st.Supply_Name = ?
+SELECT
+  sr.Supply_ID,
+  sr.Supply_Type_ID,
+  sr.SR_Date,
+  st.Supply_Name,
+  st.Unit,
+  sr.Added,
+  sr.Consumed,
+  sr.Retrieved
+FROM
+  Supply_Record sr
+  JOIN Supply_Type st ON sr.Supply_Type_ID = st.Supply_Type_ID
+WHERE
+  st.Supply_Name = ?
   AND sr.SR_Date = ?;
 
 -- Update Retrieved column of a specific record
 UPDATE Supply_Record
-SET Retrieved = TRUE
-WHERE Retrieved = FALSE
+SET
+  Retrieved = TRUE
+WHERE
+  Retrieved = FALSE
   AND sr.SR_Date = ?
   AND st.Supply_Name = ?;
