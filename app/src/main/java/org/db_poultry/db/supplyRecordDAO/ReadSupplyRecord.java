7dba05395e8a625e6e1c56d57f73b3cf59b843f6
Zhean Ganituen <zr.gntn@gmail.com>
2025-06-09 13:38:59 -0400

    Refactored View and added getFromName

--- app/src/main/java/org/db_poultry/db/supplyRecordDAO/ReadSupplyRecord.java
+++ app/src/main/java/org/db_poultry/db/supplyRecordDAO/ReadSupplyRecord.java
@@ -8,6 +8,33 @@ import java.util.ArrayList;
 import static org.db_poultry.errors.GenerateErrorMessageKt.generateErrorMessage;
 
 public class ReadSupplyRecord {
+
+    private static ArrayList<SupplyComplete> readList(Connection conn, PreparedStatement pstmt) {
+        try (ResultSet rs = pstmt.executeQuery()) {
+            ArrayList<SupplyComplete> records = new ArrayList<>();
+
+            while (rs.next()) {
+                int supply_id = rs.getInt("Supply_ID");
+                int supply_type_id = rs.getInt("Supply_Type_ID");
+                Date srDate = rs.getDate("SR_Date");
+                String supply_name = rs.getString("Supply_Name");
+                String unit = rs.getString("Unit");
+                float added = rs.getFloat("Added");
+                float consumed = rs.getFloat("Consumed");
+                boolean retrieved = rs.getBoolean("Retrieved");
+
+                records.add(new SupplyComplete(supply_id, supply_type_id, srDate, supply_name, unit, added, consumed, retrieved));
+            }
+            return records;
+        } catch (SQLException e) {
+            generateErrorMessage("Error in `readList()` in `ReadSupplyRecord`.",
+                    "SQL Exception error occurred",
+                    "",
+                    e);
+            return null;
+        }
+    }
+
     public static ArrayList<SupplyComplete> getFromDate(Connection conn, Date date) {
 
         String sql = "SELECT sr.Supply_ID, sr.Supply_Type_ID, sr.SR_Date, st.Supply_Name, st.Unit, " +
@@ -18,26 +45,31 @@ public class ReadSupplyRecord {
 
         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setDate(1, date);
-            try (ResultSet rs = pstmt.executeQuery()) {
-                ArrayList<SupplyComplete> records = new ArrayList<>();
 
-                while (rs.next()) {
-                    int supply_id = rs.getInt("Supply_ID");
-                    int supply_type_id = rs.getInt("Supply_Type_ID");
-                    Date srDate = rs.getDate("SR_Date");
-                    String supply_name = rs.getString("Supply_Name");
-                    String unit = rs.getString("Unit");
-                    float added = rs.getFloat("Added");
-                    float consumed = rs.getFloat("Consumed");
-                    boolean retrieved = rs.getBoolean("Retrieved");
-
-                    records.add(new SupplyComplete(supply_id, supply_type_id, srDate, supply_name, unit, added, consumed, retrieved));
-                }
-                return records;
-            }
+            return readList(conn, pstmt);
 
         } catch (SQLException e) {
             generateErrorMessage("Error in `getFromDate()` in `ReadSupplyRecord`.",
+                    "SQL Exception error occurred",
+                    "",
+                    e);
+            return null;
+        }
+    }
+
+    public static ArrayList<SupplyComplete> getFromName(Connection conn, String supplyName) {
+        String sql = "SELECT sr.Supply_ID, sr.Supply_Type_ID, sr.SR_Date, st.Supply_Name, st.Unit, " +
+                "sr.Added, sr.Consumed, sr.Retrieved " +
+                "FROM Supply_Record sr " +
+                "JOIN Supply_Type st ON sr.Supply_Type_ID = st.Supply_Type_ID " +
+                "WHERE st.Supply_Name = ?;";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, supplyName);
+
+            return readList(conn, pstmt);
+        } catch (SQLException e) {
+            generateErrorMessage("Error in `getFromName()` in `ReadSupplyRecord`.",
                     "SQL Exception error occurred",
                     "",
                     e);
