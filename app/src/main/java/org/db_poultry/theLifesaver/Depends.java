96a971afb5b5da8c84ae6ba17235b153001425b7
Zhean Ganituen <zr.gntn@gmail.com>
2025-06-12 17:29:00 -0400

    Added dependency tracking, automatic installation, and PostgreSQL configuration at the first execution of the DBMS. I also added some preliminary documentation regading the Shipping process of our application.

---
+++ app/src/main/java/org/db_poultry/theLifesaver/Depends.java
@@ -0,0 +1,12 @@
+package org.db_poultry.theLifesaver;
+
+public class Depends {
+    // the filenames of the dependencies inside the app/installer directory
+    // don't add the directory `installer/` as `installer\\` since we will append that
+    // in their getter functions
+    private static final String postgres = "postgresql-17.5.exe";
+
+    public static String getPostgres() {
+        return "installer\\" + postgres;
+    }
+}
--- app/src/main/java/org/db_poultry/theLifesaver/TL.java
+++ app/src/main/java/org/db_poultry/theLifesaver/TL.java
@@ -1,11 +1,12 @@
 package org.db_poultry.theLifesaver;
 
+import org.db_poultry.App;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.sql.Connection;
+import java.util.List;
 
 import static org.db_poultry.db.InitializeKt.cleanTables;
 import static org.db_poultry.errors.GenerateErrorMessageKt.generateErrorMessage;
@@ -21,7 +22,8 @@ import static org.db_poultry.errors.GenerateErrorMessageKt.generateErrorMessage;
  */
 public class TL {
     /**
-     * deletes backup folder and the config file, essentially makes it so that the next open of the DBMS is the first
+     * deletes backup folder and the config file, essentially makes
+     * it so that the next open of the DBMS is the first
      * open
      */
     public static void wipe() {
@@ -45,9 +47,9 @@ public class TL {
                         return FileVisitResult.CONTINUE;
                     }
                 });
-                System.out.println(">>> theLifesaver has deleted the backups folder.");
+                System.out.println(">>> theLifesaver has deleted " + "the backups folder.");
             } else {
-                System.out.println(">>> theLifesaver's backups folder does not exist.");
+                System.out.println(">>> theLifesaver's backups " + "folder does not exist.");
             }
         } catch (IOException e) {
             generateErrorMessage("Error at `TL_murder` in `TL`", "Failed to delete backup folder.", "", e);
@@ -59,44 +61,97 @@ public class TL {
 
             if (Files.exists(configFilePath)) {
                 Files.delete(configFilePath);
-                System.out.println(">>> theLifesaver has deleted the config file.");
+                System.out.println(">>> theLifesaver has deleted " + "the config file.");
             } else {
-                System.out.println(">>> theLifesaver's config file does not exist.");
+                System.out.println(">>> theLifesaver's config file " + "does not exist.");
             }
         } catch (IOException e) {
             generateErrorMessage("Error at `TL_murder` in `TL`", "Failed to delete config file.", "", e);
         }
 
-        System.out.println("=================== IMPORTANT! ===================");
-        System.out.println("=== IF YOU SEE THIS IN SHIPPED CODE            ===");
-        System.out.println("=== THEN SOMEBODY MESSED UP                    ===");
-        System.out.println("=== YOU SHOULDN'T BE ABLE TO SEE THIS          ===");
-        System.out.println("=== REMEMBER TO DELETE `wipe`           ===");
-        System.out.println("=== BEFORE SHIPPING                            ===");
-        System.out.println("=================== IMPORTANT! ===================");
+        System.out.println("=================== IMPORTANT! " + "===================");
+        System.out.println("=== IF YOU SEE THIS IN SHIPPED CODE    " + "        ===");
+        System.out.println("=== THEN SOMEBODY MESSED UP            " + "        ===");
+        System.out.println("=== YOU SHOULDN'T BE ABLE TO SEE THIS  " + "        ===");
+        System.out.println("=== REMEMBER TO DELETE `wipe`          " + " ===");
+        System.out.println("=== BEFORE SHIPPING                    " + "        ===");
+        System.out.println("=================== IMPORTANT! " + "===================");
+    }
+
+    private static void TL_init_postgres(String username, String password) {
+        // check if postgres exists
+        try {
+            Process p = new ProcessBuilder("cmd", "/c", "where " + "psql").redirectErrorStream(true).start();
+            int ret = p.waitFor();
+
+            // 0 if psql is in path (psql is downloaded)
+            // 1 otherwise
+            if (ret != 0) {
+                // download postgres using the installer inside
+                // app/installer
+                new ProcessBuilder("cmd", "/c", Depends.getPostgres()).inheritIO().start();
+            }
+
+        } catch (IOException | InterruptedException e) {
+            generateErrorMessage("Error at TL_init_postgres", "FATAL. Failed to init postgres database.", "", e);
+            throw new RuntimeException(e);
+        }
+
+        // then run psql
+        // and run the following commands inside:
+        // CREATE USER db_poultry WITH #####;
+        // CREATE DATABASE db_poultry OWNER db_poultry;
+        // GRANT ALL PRIVILEGES ON DATABASE db_poultry TO db_poultry;
+        // ALTER USER db_poultry WITH SUPERUSER;
+
+        String[] script = {
+                "CREATE USER %s WITH %s;".formatted(username, password),
+                "CREATE DATABASE %s OWNER %s;".formatted(username, username),
+                "GRANT ALL PRIVILEGES %s ON  %sDATABASE;".formatted(username, username),
+                "ALTER USER %s WITH SUPERUSER;".formatted(username)
+        };
+
+        try {
+            Path ts = Files.createTempFile("db_init", ".sql");
+            Files.write(ts, List.of(script));
+
+            ProcessBuilder pb = new ProcessBuilder("psql", "-U", "postgres", "-f", ts.toString());
+            pb.environment().put("PGPASSWORD", password);
+            pb.redirectErrorStream(true);
+            pb.inheritIO().start();
+        } catch  (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
-     * checks if it is the first open and if it is create the configuration file.
+     * checks if it is the first open and if it is create the
+     * configuration file.
      */
-    public static void TL_firstOpen(Connection conn) {
-        // the basis of checking for the "first open" of the DBMS is if the `.config.dbpoultry` exists
+    public static void TL_firstOpen(App app) {
+        // the basis of checking for the "first open" of the DBMS
+        // is if the `.config.dbpoultry` exists
         if (!Files.exists(Paths.get(Variables.getDotConfigPath()))) {
             // if it doesn't. Make it immediately!
-            System.out.println(">>> theLifesaver detects: first open.");
+            System.out.println(">>> theLifesaver detects: first " + "open.");
             Config.TL_writeConfig(Util.getDateNow());
         } else {
             return;
         }
 
+        // initialize postgresql
+        TL_init_postgres(app.getDatabaseName(), app.getDatabasePass());
+
         // make the backup folder
         Backup.TL_makeBackupFolder();
 
         // if it is the first open, we will clean all tables
-        cleanTables(conn);
+        cleanTables(app.getConnection());
 
         // we also want to set the PostgreSQL privileges
-        // TODO: make commandline calls here to give PostgreSQL privilages
-        // do we even need that? that's like too complicated i think...
+        // TODO: make commandline calls here to give PostgreSQL
+        //  privilages
+        // do we even need that? that's like too complicated i
+        // think...
     }
 }
