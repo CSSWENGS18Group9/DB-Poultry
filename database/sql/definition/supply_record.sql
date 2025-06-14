92a4a4b5df44204aaf7469c07ecfbebbbe9d927c
Zhean Ganituen <zr.gntn@gmail.com>
2025-06-14 16:04:24 -0400

    Added more precision for supply_record

--- database/sql/definition/supply_record.sql
+++ database/sql/definition/supply_record.sql
@@ -3,8 +3,8 @@ CREATE TABLE Supply_Record
     Supply_ID      SERIAL PRIMARY KEY,
     Supply_Type_ID INT  NOT NULL,
     SR_Date        DATE NOT NULL,
-    Added          NUMERIC(9, 4),
-    Consumed       NUMERIC(9, 4),
+    Added          NUMERIC(12, 4),
+    Consumed       NUMERIC(12, 4),
     Retrieved      BOOLEAN,
     FOREIGN KEY (Supply_Type_ID) REFERENCES Supply_Type (Supply_Type_ID) ON DELETE CASCADE,
     UNIQUE (Supply_Type_ID, SR_Date)
