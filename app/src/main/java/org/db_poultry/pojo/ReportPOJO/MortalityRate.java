a81e98a14aa9653463ead6eb4258a3b423b7cabd
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-06-15 13:33:43 +0800

    added a MortalityRate POJO.

---
+++ app/src/main/java/org/db_poultry/pojo/ReportPOJO/MortalityRate.java
@@ -0,0 +1,13 @@
+package org.db_poultry.pojo.ReportPOJO;
+
+public class MortalityRate {
+    private final float mortalityRate;
+
+    public MortalityRate(float mortalityRate) {
+        this.mortalityRate = mortalityRate;
+    }
+
+    public float getMortalityRate() {
+        return mortalityRate;
+    }
+}
