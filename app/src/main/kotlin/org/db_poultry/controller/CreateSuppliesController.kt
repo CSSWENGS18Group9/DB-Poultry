300736025a3121e16b1bdebfb0b657315510de1f
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-06-18 17:40:49 +0800

    added confirm() to CreateSuppliesController.kt

--- app/src/main/kotlin/org/db_poultry/controller/CreateSuppliesController.kt
+++ app/src/main/kotlin/org/db_poultry/controller/CreateSuppliesController.kt
@@ -7,6 +7,8 @@ import javafx.scene.control.TextField
 import javafx.scene.layout.AnchorPane
 import javafx.scene.shape.Rectangle
 import javafx.scene.text.Text
+import org.db_poultry.db.DBConnect.getConnection
+import org.db_poultry.db.supplyTypeDAO.CreateSupplyType.createSupplyType
 
 class CreateSuppliesController {
 
@@ -18,6 +20,9 @@ class CreateSuppliesController {
 
     @FXML
     private lateinit var createSuppliesTextField: TextField
+
+    @FXML
+    private lateinit var createSuppliesTextFieldUnit: TextField
 
     @FXML
     private lateinit var shapeBg: Rectangle
@@ -32,7 +37,20 @@ class CreateSuppliesController {
     private lateinit var textHeader: Text
 
     @FXML
-    fun confirm(event: ActionEvent) {
+    fun confirm() {
+
+        val supplyName = createSuppliesTextField.text
+        val supplyUnit = createSuppliesTextFieldUnit.text
+
+        println("\nSupply Name: $supplyName")
+        println("Supply Unit: $supplyUnit")
+
+        if (createSupplyType(getConnection(), supplyName, supplyUnit) != null) {
+            println("Successfully created Supply type.")
+        }
+        else {
+            println("Failed to create Supply type.")
+        }
 
     }
 
