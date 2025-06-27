52777ef08186bce538859409ec0cbe914c574a6d
Zhean Ganituen <zr.gntn@gmail.com>
2025-06-27 06:05:47 -0400

    Deleted python scripts

--- app/src/perf/README.md
+++
@@ -1,6 +0,0 @@
-# DB-Poultry Performance Testing
-
-This subdirectory is the performance testing for the DB Poultry
-database application.
-
-This uses [`uv`](https://docs.astral.sh/uv/) and Python 3.13.
--- app/src/perf/generate_dummies/util.py
+++
@@ -1,6 +0,0 @@
-def generate_unique_date(used_dates, fake, start_date, end_date):
-    while True:
-        date = fake.date_between(start_date=start_date, end_date=end_date)
-        if date not in used_dates:
-            used_dates.add(date)
-            return date
