9207d06635a5fd9b2373a3a92bbb417ce323aabd
Dattebayo2505 <seankyled1st@gmail.com>
2025-07-05 10:49:09 +0800

    style: Revised supplies home

--- app/src/main/kotlin/org/db_poultry/controller/SuppliesHomeController.kt
+++ app/src/main/kotlin/org/db_poultry/controller/SuppliesHomeController.kt
@@ -1,13 +1,9 @@
 package org.db_poultry.controller
 
-import org.db_poultry.util.SceneSwitcher
 import org.db_poultry.util.GeneralUtil
 
-import javafx.event.ActionEvent
 import javafx.fxml.FXML
-import javafx.scene.control.Button
 import javafx.scene.layout.AnchorPane
-import javafx.scene.text.Text
 
 class SuppliesHomeController {
 
@@ -15,34 +11,22 @@ class SuppliesHomeController {
     private lateinit var homeAnchorPane: AnchorPane
 
     @FXML
-    private lateinit var homeSuppliesBtn1: Button
-
-    @FXML
-    private lateinit var homeSuppliesBtn2: Button
-
-    @FXML
-    private lateinit var homeSuppliesBtn3: Button
-
-    @FXML
-    private lateinit var txtHomeMenu1: Text
-
-    @FXML
-    fun switchToMenu(event: ActionEvent) {
+    fun navigateToMenu() {
         GeneralUtil.loadContentView(homeAnchorPane, "/fxml/content_home.fxml")
     }
 
     @FXML
-    fun switchToCreate(event: ActionEvent) {
+    fun navigateToCreate() {
         GeneralUtil.loadContentView(homeAnchorPane, "/fxml/content_create_supplies.fxml")
     }
 
     @FXML
-    fun switchToUpdate(event: ActionEvent) {
+    fun navigateToUpdate() {
         GeneralUtil.loadContentView(homeAnchorPane, "/fxml/content_home_supplies_grid.fxml")
     }
 
     @FXML
-    fun switchToView(event: ActionEvent) {
+    fun navigateToView() {
         GeneralUtil.loadContentView(homeAnchorPane, "/fxml/content_view_supplies.fxml")
     }
 
