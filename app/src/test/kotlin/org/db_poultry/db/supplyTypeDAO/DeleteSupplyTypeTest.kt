41a0c90049fc019c00331b3b8a28859411cc390c
OutForMilks <stephen_borja_a@dlsu.edu.ph>
2025-07-06 15:50:37 +0800

    Create DeleteSupplyTypeTest.kt

---
+++ app/src/test/kotlin/org/db_poultry/db/supplyTypeDAO/DeleteSupplyTypeTest.kt
@@ -0,0 +1,77 @@
+package org.db_poultry.db.supplyTypeDAO
+
+import org.db_poultry.db.supplyRecordDAO.*
+import org.db_poultry.App
+import org.db_poultry.db.DBConnect
+import org.db_poultry.db.cleanTables
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+import java.sql.Connection
+import java.sql.Date
+
+class DeleteSupplyTypeTest {
+    private var jdbcURL: String
+    private var conn: Connection
+
+    init {
+        val app = App()
+        app.getDotEnv()
+        jdbcURL = "jdbc:postgresql://localhost:${app.databasePort}/${app.databaseName}"
+        DBConnect.init(jdbcURL, app.databaseName, app.databasePass)
+        conn = DBConnect.getConnection()!!
+        cleanTables(conn)
+    }
+
+    @Test
+    fun testDeleteTypeWithDataOne() {
+        val date = Date.valueOf("2025-01-02")
+
+        CreateSupplyType.createSupplyType(conn, "Test_1", "kg")
+        CreateSupplyType.createSupplyType(conn, "Test_2", "kg")
+
+        val result = DeleteSupplyType.undoCreateSupplyType(conn)
+
+        assertEquals("DELETE FROM Supply_Type ORDER BY Supply_Type_ID DESC LIMIT 1", result)
+        assertNull(ReadSupplyRecord.getOneByDateAndName(conn, date, "Test_2"))
+    }
+
+    @Test
+    fun testDeleteTypeWithDataTwo() {
+        val dateOne = Date.valueOf("2025-01-03")
+        val dateTwo = Date.valueOf("2025-01-02")
+
+        CreateSupplyType.createSupplyType(conn, "Test_1", "kg")
+        CreateSupplyType.createSupplyType(conn, "Test_2", "kg")
+
+
+        CreateSupplyRecord.createSupplyRecord(
+            conn,
+            1,
+            dateOne,
+            BigDecimal("200.00"),
+            BigDecimal("20.00"),
+            false
+        )
+
+        CreateSupplyRecord.createSupplyRecord(
+            conn,
+            2,
+            dateTwo,
+            BigDecimal("100.00"),
+            BigDecimal("50.00"),
+            false
+        )
+
+        val result = DeleteSupplyType.undoCreateSupplyType(conn)
+
+        assertEquals("DELETE FROM Supply_Type ORDER BY Supply_Type_ID DESC LIMIT 1", result)
+        assertNull(ReadSupplyRecord.getOneByDateAndName(conn, dateTwo, "Test_2"))
+    }
+
+    @Test
+    fun testCreateDeleteTypeWithNoData() {
+        val result = DeleteSupplyType.undoCreateSupplyType(conn)
+        assertNull(result)
+    }
+}
