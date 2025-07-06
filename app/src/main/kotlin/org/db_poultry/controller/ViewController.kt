4c071d5c110cb7e173458fb7b96fca5f12fb142a
Dattebayo2505 <seankyled1st@gmail.com>
2025-07-06 23:58:06 +0800

    style: Revise flock selection UI with grid layout and new button styles (backend)

--- app/src/main/kotlin/org/db_poultry/controller/ViewController.kt
+++ app/src/main/kotlin/org/db_poultry/controller/ViewController.kt
@@ -1,18 +1,20 @@
 package org.db_poultry.controller
 
 import org.db_poultry.util.GeneralUtil
-import org.db_poultry.util.flockDateSingleton
 
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
-import javafx.scene.control.ChoiceBox
+import javafx.geometry.Pos
+import javafx.scene.control.Button
 import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.FlowPane
 import org.db_poultry.db.DBConnect
 import org.db_poultry.db.flockDAO.ReadFlock
+import org.db_poultry.pojo.FlockPOJO.FlockComplete
 import java.net.URL
 import java.sql.Date
+import java.time.LocalDate
 import java.util.ResourceBundle
-import kotlin.collections.isNotEmpty
 
 // TODO: Remove dropdown, replace with gridlike structure @Dattebayo2505
 class ViewController: Initializable {
@@ -21,32 +23,38 @@ class ViewController: Initializable {
     lateinit var selectFlockAnchorPane: AnchorPane
 
     @FXML
-    private lateinit var viewSelectFlockChoiceBox: ChoiceBox<Any>
-
-    val data = flockDateSingleton.instance
+    lateinit var mainFlowPane: FlowPane
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
+        loadFlockGrid()
+    }
 
+    private fun loadFlockGrid() {
         val flockMap = ReadFlock.allByID(DBConnect.getConnection())
 
-        val dateList = mutableListOf<String>()
-
         if (flockMap != null && flockMap.isNotEmpty()) {
-            val dates = flockMap.values.map { it.flock.startingDate.toString() }
-            dateList.addAll(dates)
-            println("Flock starting dates: $dates") // DEBUGGING
+            for ((flockId, flockComplete) in flockMap) {
+                val button = createFlockButton(flockComplete)
+                mainFlowPane.children.add(button)
+            }
         } else {
-            println("No data found.")
+            println("No flock records found")
         }
-
-        viewSelectFlockChoiceBox.items.clear()
-        viewSelectFlockChoiceBox.items.addAll(dateList)
-        viewSelectFlockChoiceBox.value = "Select a flock"
     }
 
-    @FXML
-    fun switchToViewFlockDetails() {
-        data.setDate(Date.valueOf(viewSelectFlockChoiceBox.value as String))
-        GeneralUtil.navigateToMainContent(selectFlockAnchorPane, "/fxml/content_view_flock_details.fxml")
+    private fun createFlockButton(flockComplete: FlockComplete): Button {
+        val flock = flockComplete.flock
+        val formattedDate = GeneralUtil.formatDatePretty(flock.startingDate.toLocalDate())
+
+        return Button(formattedDate).apply {
+            alignment = Pos.CENTER
+            maxHeight = -1.0
+            maxWidth = -1.0
+            isMnemonicParsing = false
+            prefWidth = 225.0
+            styleClass.addAll("flock-button", "h4-bold")
+
+        }
     }
+
 }
---
+++ app/src/main/kotlin/org/db_poultry/controller/backend/CurrentFlockInUse.kt
@@ -0,0 +1,6 @@
+package org.db_poultry.controller.backend
+
+class CurrentFlockInUse {
+
+
+}
