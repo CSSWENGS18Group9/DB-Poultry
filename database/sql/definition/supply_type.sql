158d496212be076939f36f9713735373123665f2
Zhean Ganituen <zr.gntn@gmail.com>
2025-07-10 23:26:09 -0400

    Revised SQL definition for Supply_Type table

--- database/sql/definition/supply_type.sql
+++ database/sql/definition/supply_type.sql
@@ -1,6 +1,7 @@
 CREATE TABLE
     Supply_Type (
         Supply_Type_ID SERIAL PRIMARY KEY,
-        Supply_Name TEXT UNIQUE NOT NULL CHECK (Supply_Name <> ''),
+        Supply_Name VARCHAR(36) UNIQUE NOT NULL CHECK (Supply_Name <> ''), -- should be enough for any supply_name
         Unit VARCHAR(12) NOT NULL CHECK (Unit <> '') -- should be enough for any unit of measurement
+        Image_File_Path VARCHAR(255) UNIQUE,
     );
