a89e75a517c688f3b45d23ed3a127b176b3db838
Zhean Ganituen <zr.gntn@gmail.com>
2025-07-10 23:27:40 -0400

    Updated cleanTables to reflect changes

--- app/src/main/kotlin/org/db_poultry/db/Initialize.kt
+++ app/src/main/kotlin/org/db_poultry/db/Initialize.kt
@@ -36,8 +36,9 @@ fun cleanTables(conn: Connection?) {
 
         "Supply_Type" to """
             Supply_Type_ID SERIAL PRIMARY KEY,
-            Supply_Name TEXT UNIQUE NOT NULL CHECK (Supply_Name <> ''),
-            Unit VARCHAR(12) NOT NULL CHECK (Unit <> '')
+            Supply_Name VARCHAR(36) UNIQUE NOT NULL CHECK (Supply_Name <> ''),
+            Unit VARCHAR(12) NOT NULL CHECK (Unit <> ''),
+            Image_File_Path VARCHAR(255),
         """.trimIndent(),
 
         "Supply_Record" to """            
