c4ea4665f334550463a12cc1fbc09a469e0443ef
Jaztin Jimenez <jaztin_jacob_jimenez@dlsu.edu.ph>
2025-09-08 15:26:25 +0800

    Update supply_record.sql

--- database/sql/definition/supply_record.sql
+++ database/sql/definition/supply_record.sql
@@ -7,6 +7,7 @@ CREATE TABLE Supply_Record
     Consumed       NUMERIC(12, 4),
     Current_Count   NUMERIC(12, 4),
     Retrieved      BOOLEAN,
+    Price         NUMERIC(12, 4),
     FOREIGN KEY (Supply_Type_ID) REFERENCES Supply_Type (Supply_Type_ID) ON DELETE CASCADE,
     UNIQUE (Supply_Type_ID, SR_Date)
 );
