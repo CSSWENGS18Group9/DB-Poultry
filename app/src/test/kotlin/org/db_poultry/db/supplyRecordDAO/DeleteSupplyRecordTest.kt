4fb15fced2e9f38736610e852ae47937a7ef20bc
OutForMilks <stephen_borja_a@dlsu.edu.ph>
2025-09-19 18:36:55 +0800

    Only commented out the inside of the object for deletesupplyrecordtest

--- app/src/test/kotlin/org/db_poultry/db/supplyRecordDAO/DeleteSupplyRecordTest.kt
+++ app/src/test/kotlin/org/db_poultry/db/supplyRecordDAO/DeleteSupplyRecordTest.kt
@@ -1,23 +1,23 @@
-//package org.db_poultry.db.supplyRecordDAO
-//
-//import org.db_poultry.db.DBConnect
-//import org.db_poultry.db.cleanTables
-//import org.db_poultry.db.supplyTypeDAO.CreateSupplyType
-//import org.junit.jupiter.api.Assertions.assertEquals
-//import org.junit.jupiter.api.Assertions.assertNull
-//import org.junit.jupiter.api.Test
-//import java.math.BigDecimal
-//import java.sql.Connection
-//import java.sql.Date
-//
-///*
-//Check if:
-//working
-//will work if the latest thing was a month ago
-//will work if there is no data
-// */
-//
-//class DeleteSupplyRecordTest {
+package org.db_poultry.db.supplyRecordDAO
+
+import org.db_poultry.db.DBConnect
+import org.db_poultry.db.cleanTables
+import org.db_poultry.db.supplyTypeDAO.CreateSupplyType
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
+import java.sql.Connection
+import java.sql.Date
+
+/*
+Check if:
+working
+will work if the latest thing was a month ago
+will work if there is no data
+ */
+
+class DeleteSupplyRecordTest {
 //    private val jdbcURL = "jdbc:postgresql://localhost:5432/db_poultry_test"
 //    private val conn: Connection
 //
@@ -60,40 +60,40 @@
 //        assertEquals("DELETE FROM Supply_Record ORDER BY Supply_ID DESC LIMIT 1", result)
 //        assertNull(ReadSupplyRecord.getOneByDateAndName(conn, date, "Test_2"))
 //    }
+
+//    @Test
+//    fun testDeleteRecordWithDataTwo() {
+//        val dateOne = Date.valueOf("2025-01-03")
+//        val dateTwo = Date.valueOf("2025-01-02")
 //
-////    @Test
-////    fun testDeleteRecordWithDataTwo() {
-////        val dateOne = Date.valueOf("2025-01-03")
-////        val dateTwo = Date.valueOf("2025-01-02")
-////
-////        CreateSupplyType.createSupplyType(conn, "Test_1", "kg", "src/main/resources/img/supply-img/Apog.png", "src/main/resources/img/supply-img/default.png")
-////        CreateSupplyType.createSupplyType(conn, "Test_2", "kg", "src/main/resources/img/supply-img/Apog.png", "src/main/resources/img/supply-img/default.png")
-////
-////
-////        CreateSupplyRecord.createSupplyRecord(
-////            conn,
-////            1,
-////            dateOne,
-////            BigDecimal("200.00"),
-////            BigDecimal("20.00"),
-////            false,
-////            BigDecimal("50.00")
-////        )
-////
-////        CreateSupplyRecord.createSupplyRecord(
-////            conn,
-////            2,
-////            dateTwo,
-////            BigDecimal("100.00"),
-////            BigDecimal("50.00"),
-////            false,
-////            BigDecimal("50.00")
-////        )
-////
-////        val result = DeleteSupplyRecord.undoCreateSupplyRecord(conn)
-////
-////        assertEquals("DELETE FROM Supply_Record ORDER BY Supply_ID DESC LIMIT 1", result)
-////        assertNull(ReadSupplyRecord.getOneByDateAndName(conn, dateTwo, "Test_2"))
-////    }
+//        CreateSupplyType.createSupplyType(conn, "Test_1", "kg", "src/main/resources/img/supply-img/Apog.png", "src/main/resources/img/supply-img/default.png")
+//        CreateSupplyType.createSupplyType(conn, "Test_2", "kg", "src/main/resources/img/supply-img/Apog.png", "src/main/resources/img/supply-img/default.png")
 //
-//}
+//
+//        CreateSupplyRecord.createSupplyRecord(
+//            conn,
+//            1,
+//            dateOne,
+//            BigDecimal("200.00"),
+//            BigDecimal("20.00"),
+//            false,
+//            BigDecimal("50.00")
+//        )
+//
+//        CreateSupplyRecord.createSupplyRecord(
+//            conn,
+//            2,
+//            dateTwo,
+//            BigDecimal("100.00"),
+//            BigDecimal("50.00"),
+//            false,
+//            BigDecimal("50.00")
+//        )
+//
+//        val result = DeleteSupplyRecord.undoCreateSupplyRecord(conn)
+//
+//        assertEquals("DELETE FROM Supply_Record ORDER BY Supply_ID DESC LIMIT 1", result)
+//        assertNull(ReadSupplyRecord.getOneByDateAndName(conn, dateTwo, "Test_2"))
+//    }
+
+}
