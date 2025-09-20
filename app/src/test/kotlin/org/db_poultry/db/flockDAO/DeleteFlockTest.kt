eb2ffcd3f9247b0ed576de963d0807924c6bcf2e
Zhean Ganituen <zr.gntn@gmail.com>
2025-09-21 05:16:18 +0800

    Added `cleanAndInitTables()` in DeleteFlockTest.kt

--- app/src/test/kotlin/org/db_poultry/db/flockDAO/DeleteFlockTest.kt
+++ app/src/test/kotlin/org/db_poultry/db/flockDAO/DeleteFlockTest.kt
@@ -3,6 +3,7 @@ package org.db_poultry.db.flockDAO
 import org.db_poultry.db.DBConnect
 import org.db_poultry.db.initDBAndUser
 import org.db_poultry.db.initTables
+import org.db_poultry.db.cleanAndInitTables
 import org.junit.jupiter.api.Test
 import java.sql.Connection
 import java.sql.Date
@@ -20,19 +21,19 @@ class DeleteFlockTest {
         conn = DBConnect.getConnection()!!
 
         initTables(conn)
-}
+    }
 
     @Test
     fun testDeleteFlockWithDataOne() {
-        val date =      Date.valueOf("1000-01-01")
+        val date = Date.valueOf("1000-01-01")
         CreateFlock.createFlock(conn, 100, date)
 
         val result = DeleteFlock.undoCreateFlock(conn)
 
         assertEquals("DELETE FROM Flock WHERE ctid IN (SELECT ctid FROM Flock ORDER BY flock_id DESC LIMIT 1)", result)
         assertNull(ReadFlock.getFlockFromADate(conn, date))
-    cleanAndInitTables(conn)
-}
+        cleanAndInitTables(conn)
+    }
 
     fun testDeleteFlockWithDataTwo() {
         val dateOne = Date.valueOf("1000-01-02")
@@ -45,14 +46,13 @@ class DeleteFlockTest {
 
         assertEquals("DELETE FROM Flock ORDER BY flock_id DESC LIMIT 1", result)
         assertNull(ReadFlock.getFlockFromADate(conn, dateTwo))
-    cleanAndInitTables(conn)
-}
+        cleanAndInitTables(conn)
+    }
 
     fun testDeleteFlockWithNoData() {
         val result = DeleteFlock.undoCreateFlock(conn)
 
         assertNull(result)
-    cleanAndInitTables(conn)
-}
-cleanAndInitTables(conn)
+        cleanAndInitTables(conn)
+    }
 }
