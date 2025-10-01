7b1c4fa10df2217558a32d34409fa3106ef55033
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-10-01 18:27:45 +0800

    uncommented

--- app/src/test/kotlin/org/db_poultry/db/supplyRecordDAO/ReadSupplyRecordTest.kt
+++ app/src/test/kotlin/org/db_poultry/db/supplyRecordDAO/ReadSupplyRecordTest.kt
@@ -88,8 +88,8 @@ class ReadSupplyRecordTest {
 
         assertEquals(13, first.supply_type_id)
         assertEquals(date, first.date)
-//        assertEquals(BigDecimal("300.0000"), first.added) // Error here
-//        assertEquals(BigDecimal("30.0000"), first.consumed) // Error here
+        assertEquals(BigDecimal("300.0000"), first.added)
+        assertEquals(BigDecimal("30.0000"), first.consumed)
         assertEquals(false, first.isRetrieved)
         assertEquals(BigDecimal("45.0000"), first.price)
 
